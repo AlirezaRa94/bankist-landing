177980f05b57cf2b96bb1fe8115b2dd85b068b58
Alireza Raei <alirezaraei17@gmail.com>
2024-01-22 20:18:40 +1100

    feat: Add smooth scrolling functionality
    Introduce smooth scrolling behavior to the "Learn more" button. When the button with the class 'btn--scroll-to' is clicked, the page will smoothly scroll to the section with the ID 'section--1'.

--- script.js
+++ script.js
@@ -29,3 +29,12 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////
+// Scrolling by clicking on Learn more
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function () {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
